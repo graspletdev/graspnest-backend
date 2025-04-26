bd7494bc3d40dfe719724981e3d09602a85f0da5
ravi-kumar <ravik_29@apple.com>
2025-04-26 12:52:48 +0100

    admin format

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { DashboardDto } from './admin.dto';
 import { AuthGuard, RoleGuard, Roles, Public } from 'nest-keycloak-connect';
 import { ApiResponse } from 'src/model/apiresponse.model';
 
 @Controller('api/admin')
-// @UseGuards(AuthGuard, RoleGuard)
+@UseGuards(AuthGuard, RoleGuard)
 export class AdminController {
     constructor(private readonly adminService: AdminService) {}
 
     @Get('dashboard')
-    //     @Roles({ roles: ['SuperAdmin'] })
-    @Public()
-    async dashboard(): Promise<ApiResponse<DashboardDto>> {
+    @Roles({ roles: ['SuperAdmin'] })
+//    @Public()
+    async dashboard(@Request() req): Promise<ApiResponse<DashboardDto>> {
+        //console.log(req)
         const data = await this.adminService.getDashboard();
         return {
             result: true,
